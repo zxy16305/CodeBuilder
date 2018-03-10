838050632d93d41919b05e870ee3c0824d5006c1
Kiswich <zxy16305@gmail.com>
2018-03-10 13:22:24 +0800

    环境搭建，编写从数据库到前端的一些力模板

---
+++ src/test/java/top/kiswich/codebuilder/CodeBuilderApplicationTests.java
@@ -0,0 +1,16 @@
+package top.kiswich.codebuilder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeBuilderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
